0b1c74e992b12b32d3d02aa03d3e4ffed89a567f
zexhior <herizoras1@gmail.com>
2026-02-27 18:41:50 +0300

    chore: move id into base for all model

--- src/model/base.py
+++ src/model/base.py
@@ -4,6 +4,7 @@ from src.config.env import PG_SCHEMA
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
+    id: Mapped[int] = mapped_column(primary_key=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime, nullable=False, server_default=func.now(), onupdate=func.now()
     )
