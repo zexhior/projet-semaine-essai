c0a1112354b7a4a1d4ea3b60d60ea603b3bb02fd
zexhior <herizoras1@gmail.com>
2026-03-13 16:13:36 +0300

    chore: create model connexion

---
+++ src/model/connexion.py
@@ -0,0 +1,10 @@
+from src.model.base import BaseConnexion
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+class Connexion(BaseConnexion):
+    __tablename__ = 'connexions'
+
+    id = Column(Integer, primary_key=True)
+    source: Mapped[str] = mapped_column(String(255), nullable=False)
+    connected_at: Mapped[str] = mapped_column(String(255), nullable=False)
