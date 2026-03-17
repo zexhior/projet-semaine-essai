88098fa900e37f036399af87b95abc3eecc321bc
zexhior <herizoras1@gmail.com>
2026-03-17 07:07:30 +0300

    feat: merge connexion and transaction project

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ from src.model.suivi import (
     TransactionGrossisteLivreur,
     SoldeAgents
 )
+from src.model.connexion import Connexion
 
 # Add src directory to Python path
 sys.path.insert(0, str(Path(__file__).parent))
@@ -46,11 +47,31 @@ class Main():
         live_services = self.session.query(LiveService).all()
         for service in live_services:
             print(service)
+            
+import socket
+
+def is_connected():
+    try:
+        socket.create_connection(("8.8.8.8", 53), timeout=5)
+        return True
+    except OSError:
+        return False
         
 if __name__ == "__main__":
     engine = None  # Replace with your actual engine initialization
     app = Main(engine)
     app.run()
-    # app.read()
-    # app.stop()
-    # app.read()
+    
+    from datetime import datetime
+    import time
+
+    while True:
+        if is_connected():
+            connexion = Connexion(source='application', status='connected',
+            description='Internet connecté', checked_at=datetime.now())
+            session.add(connexion)
+            session.commit()
+        else:
+            print("Internet perdu")
+
+        time.sleep(10)
