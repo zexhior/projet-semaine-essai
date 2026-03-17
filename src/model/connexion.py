dd8705b1891ce6f0aeb62bc1d2f70e18ba52eb48
zexhior <herizoras1@gmail.com>
2026-03-17 07:07:45 +0300

    feat: create model connexion

--- src/model/connexion.py
+++ src/model/connexion.py
@@ -1,11 +1,13 @@
 from src.model.base import BaseConnexion
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 
 class Connexion(BaseConnexion):
     __tablename__ = 'connexions'
     __table_args__ = {"schema": "connexion"}
 
-    id = Column(Integer, primary_key=True)
-    source: Mapped[str] = mapped_column(String(255), nullable=False)
-    connected_at: Mapped[str] = mapped_column(String(255), nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    source: Mapped[str] = mapped_column(String(100), default='application')
+    status: Mapped[str] = mapped_column(String(20))
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
+    checked_at: Mapped[DateTime] = mapped_column(DateTime) 
